3a4da54aa891dd5f09a3ddb587407a4b9f02c2a6
Vladyslav <vladnpr@gmail.com>
2023-07-04 13:32:17 +0300

    goroutines

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var randChan = make(chan int)
+	var averageChan = make(chan float64)
+
+	for i := 0; i <= 100; i++ {
+		go func() {
+			randChan <- rand.Intn(100)
+		}()
+
+		go func() {
+			var nums []int
+			var sum int
+
+			for num := range randChan {
+				nums = append(nums, num)
+				for _, val := range nums {
+					sum += val
+				}
+				averageChan <- float64(sum) / float64(len(nums))
+			}
+		}()
+
+		go func() {
+			fmt.Printf("\nСереднє значення: %.3f", <-averageChan)
+		}()
+	}
+}
