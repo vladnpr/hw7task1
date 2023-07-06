f1525aa5c37ef3d318daca0f7c5abf06ac11dec5
Vladyslav <vladnpr@gmail.com>
2023-07-06 11:32:00 +0300

    fixes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
@@ -10,25 +11,42 @@ func main() {
 	var averageChan = make(chan float64)
 	var nums []int
 
-	for i := 0; i <= 100; i++ {
-		go func() {
+	go func() {
+		for i := 0; i < 3; i++ {
 			randChan <- rand.Intn(100)
-		}()
+		}
+	}()
 
-		go func() {
-			var sum int
+	go func() {
+		var sum int
 
-			for num := range randChan {
+		for {
+			select {
+			case num := <-randChan:
 				nums = append(nums, num)
+
 				for _, val := range nums {
 					sum += val
 				}
-				averageChan <- float64(sum) / float64(len(nums))
-			}
-		}()
 
-		go func() {
-			fmt.Printf("\nСереднє значення: %.3f", <-averageChan)
-		}()
-	}
+				averageChan <- float64(sum) / float64(len(nums))
+
+			default:
+				continue
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case av := <-averageChan:
+				fmt.Printf("\nСереднє значення: %.3f", av)
+			default:
+				continue
+			}
+		}
+	}()
+
+	time.Sleep(3 * time.Second)
 }
